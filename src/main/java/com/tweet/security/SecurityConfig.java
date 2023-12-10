b6dd2dd36d27c3f9bd0364c28e328754992ad33e
Archit Gupta <architg564@gmail.com>
2023-12-11 00:02:29 +0530

    production changes applied

--- src/main/java/com/tweet/security/SecurityConfig.java
+++ src/main/java/com/tweet/security/SecurityConfig.java
@@ -38,7 +38,7 @@ public class SecurityConfig {
                     @Override
                     public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
                         CorsConfiguration config = new CorsConfiguration();
-                        config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+                        config.setAllowedOrigins(Collections.singletonList("https://user-tweets.github.io"));
                         config.setAllowedMethods(Collections.singletonList("*"));
                         config.setAllowCredentials(true);
                         config.setAllowedHeaders(Collections.singletonList("*"));
